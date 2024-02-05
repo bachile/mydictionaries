a5dcc91e3733865bc665b58a1f12d462af9ed40a
Brando <brando_lezzana1@baylor.edu>
2024-02-05 15:30:00 -0600

    updated dictionary start file

--- 1_dictionary start file.py
+++ 1_dictionary start file.py
@@ -4,22 +4,31 @@ phonebook = {'Chris':'555−1111',
              'Katie':'555−2222',
              'Joanne':'555−3333'}
 
-
+'''
 
 print()
 print('*****  start section 1 - print dictionary ********')
 print()
 
+print(phonebook)
+print(len(phonebook))
 
+print(phonebook['Chris'])
 
+# empty dict
+my_dictionary = {}
 
+# m and n are keys, 8 and 9 are values
+my_dictionary2 = dict(m = 8, n = 9)
+
+print(my_dictionary2)
 
 print()
 print('*****  end section 1 ********')
 print()
 
 
-'''
+
 
 
 print()
@@ -27,8 +36,12 @@ print('*****  start section 2 - search dictionary ********')
 print()
 
 
+name = 'chris'
 
-
+if name in phonebook:
+    print(f'Name: {name}  Phone Number: {phonebook[name]}')
+else:
+    print(name + " is not in phonebook.")
 
 
 
@@ -47,8 +60,14 @@ print('*****  start section 3 - edit/append dictionary ********')
 print()
 
 
+print(phonebook)
 
+# adds new key value pair
+phonebook['Joe'] = '555-0123'
+# updates value assigned to Chris
+phonebook['Chris'] = '555-4444'
 
+print(phonebook)
 
 print()
 print('*****  end section 3 ********')
@@ -63,8 +82,11 @@ print()
 print('*****  start section 4 - delete/remove from dictionary ********')
 print()
 
+print(phonebook)
 
+del phonebook['Chris']
 
+print(phonebook)
 
 print()
 print('*****  end section 4 ********')
@@ -72,16 +94,18 @@ print()
 
 
 
-
+'''
 
 
 print()
 print('*****  start section 5 - iterate through keys, values, items ********')
 print()
 
+for key in phonebook:
+    print(f'The key is {key} and the value is {phonebook[key]}')
 
-
-
+for value in phonebook.values():
+    print(value)
 
 
 print()
@@ -89,7 +113,7 @@ print('*****  end section 5 ********')
 print()
 
 
-
+'''
 
 
 print()
